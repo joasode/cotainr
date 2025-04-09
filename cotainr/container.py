70821b8f09359b10f53e081bf051442ef1564297
joasode <joaso@dtu.dk>
2025-04-09 13:51:38 +0200

    Run branch in main container

--- cotainr/container.py
+++ cotainr/container.py
@@ -64,6 +64,7 @@ class SingularitySandbox:
 
     def __init__(self, *, base_image, log_settings=None):
         """Construct the SingularitySandbox context manager."""
+        # Test comment
         self.base_image = base_image
         self.sandbox_dir = None
         self.architecture = None
